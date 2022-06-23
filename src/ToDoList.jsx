7aa98639df7e60dfa44d2591e61fb3659ee907b1
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-23 22:01:24 +0530

    #1, edit ui btn

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import edit from './assets/edit.png'
 
 const ToDoLists = (props) => {
     const itemValRemove = (indexList) => {
@@ -10,11 +11,23 @@ const ToDoLists = (props) => {
         });
         props.setList(listNew);
     }
+    const itemValEdit = (indexList) => {
+        // eslint-disable-next-line array-callback-return
+        // const listNew = props.list.filter((listVal, indexVal) => {
+        //     if(indexVal !== indexList){
+        //         return listVal;
+        //     }
+        // });
+        // props.setList(listNew);
+    }
     return (
         <div className="list">
             <button key={props.index} className='button_remove' onClick={()=>{
                 itemValRemove(props.index);
             }}>X</button>
+            <button key={props.index} className='button_edit' onClick={()=>{
+                itemValEdit(props.index);
+            }}><img src={edit} className="imgEdit" alt="edit"/></button>
             <li key={props.index + 'data'}>{props.listVal}</li>
         </div>
     )
