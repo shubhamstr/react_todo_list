78891335f4e37dddb3e5a06a5fe4aa7a0962ea72
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-23 22:14:14 +0530

    simple modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import ToDoLists from "./ToDoList";
+import Modal from "react-modal";
 
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement('#root');
 function App() {
   const [val, setVal] = useState('');
   const [list, setList] = useState([]);
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
 
   const itemVal = (event) => {
     setVal(event.target.value);
@@ -43,12 +71,30 @@ function App() {
             <ol>
             {
               list.map((listVal, index) => (
-                <ToDoLists key={index} listVal={listVal} index={index} setList={setList} list={list} />
+                <ToDoLists key={index} listVal={listVal} index={index} setList={setList} list={list} openModal={openModal} />
               ))
             }
             {(list.length === 0) ? <li>No Records</li> : null}
             </ol>
         </div>
+        <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
       </div>
     </>
   );
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import edit from './assets/edit.png'
+import React, { useState } from "react";
+import edit from "./assets/edit.png";
 
 const ToDoLists = (props) => {
-    const itemValRemove = (indexList) => {
-        // eslint-disable-next-line array-callback-return
-        const listNew = props.list.filter((listVal, indexVal) => {
-            if(indexVal !== indexList){
-                return listVal;
-            }
-        });
-        props.setList(listNew);
-    }
-    const itemValEdit = (indexList) => {
-        // eslint-disable-next-line array-callback-return
-        // const listNew = props.list.filter((listVal, indexVal) => {
-        //     if(indexVal !== indexList){
-        //         return listVal;
-        //     }
-        // });
-        // props.setList(listNew);
-    }
-    return (
-        <div className="list">
-            <button key={props.index} className='button_remove' onClick={()=>{
-                itemValRemove(props.index);
-            }}>X</button>
-            <button key={props.index} className='button_edit' onClick={()=>{
-                itemValEdit(props.index);
-            }}><img src={edit} className="imgEdit" alt="edit"/></button>
-            <li key={props.index + 'data'}>{props.listVal}</li>
-        </div>
-    )
-}
+  const itemValRemove = (indexList) => {
+    // eslint-disable-next-line array-callback-return
+    const listNew = props.list.filter((listVal, indexVal) => {
+      if (indexVal !== indexList) {
+        return listVal;
+      }
+    });
+    props.setList(listNew);
+  };
+  const itemValEdit = (indexList) => {
+    console.log("edit", indexList);
+    console.log(props.list[indexList]);
+    props.openModal();
+  };
+  return (
+    <div className="list">
+      <button
+        key={props.index}
+        className="button_remove"
+        onClick={() => {
+          itemValRemove(props.index);
+        }}
+      >
+        X
+      </button>
+      <button
+        key={props.index + "e"}
+        className="button_edit"
+        onClick={() => {
+          itemValEdit(props.index);
+        }}
+      >
+        <img src={edit} className="imgEdit" alt="edit" />
+      </button>
+      <li key={props.index + "data"}>{props.listVal}</li>
+    </div>
+  );
+};
 
 export default ToDoLists;
